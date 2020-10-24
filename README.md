cf59fb41fb2415f9cf3d9d35e3b5065f8433dfe1
sil <silvana.trabalon@gmail.com>
2020-10-24 05:31:40 -0300

    project

---
+++ README.md
@@ -0,0 +1,36 @@
+# React application with NodeJS and Express server
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app). Then an Express and NodeJS server was added in the `server` directory. The server is proxied via the `proxy` key in `package.json`.
+
+## Using this project
+
+Clone the project, change into the directory and install the dependencies.
+
+```bash
+git clone https://github.com/silvanatrabalon/react-node.git
+cd react-node
+npm install
+```
+
+Create a `.env` file for environment variables in your server.
+
+You can start the server on its own with the command:
+
+```bash
+npm run server
+```
+
+Run the React application on its own with the command:
+
+```bash
+npm start
+```
+
+Run both applications together with the command:
+
+```bash
+npm run dev
+```
+
+The React application will run on port 3000 and the server port 3001.
+
---
+++ server/index.js
@@ -0,0 +1,40 @@
+//with Express
+/*
+const express = require('express');
+const app = express();
+
+app.get('/api/greeting', (req, res) => {
+  const name = req.query.name || 'World';
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+); 
+*/
+//with NodeJS
+const http = require('http');
+const url = require('url');
+const server = http.createServer((req, res) => {
+  const queryObject = url.parse(req.url,true).query;
+  switch (req.method) {
+      case "GET":
+        if (url.parse(req.url,true).pathname === '/api/greeting') {
+            const name = queryObject.name  || 'World';
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.write(JSON.stringify({ greeting: `Hello ${name}!` }));    
+            res.end();
+        }
+        break;
+      default:
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.write('404 NOT FOUND');
+          res.end('hello word');
+          break;
+  }
+});
+
+server.listen(3001, () =>
+  console.log('Node server is running on localhost:3001')
+);
